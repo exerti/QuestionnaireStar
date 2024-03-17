4eac61454094994e90f547a9238529904b899555
悲伤 <1783782539@163.om>
2024-03-17 20:27:07 +0800

    first commit

---
+++ questionnaire_Start/.prettierrc.js
@@ -0,0 +1,10 @@
+// 定义自己的preitter 相关配置
+module.exports = {
+    
+    // 箭头函数
+    "arrow-parens": 'avoid',
+    // 括号内不要出现空格
+    "no-trailing-spaces": "error",
+    // 分号
+    "semi":"error",
+}
---
+++ questionnaire_Start/src/App.tsx
@@ -0,0 +1,13 @@
+import List1 from "./commonpent/List";
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      <List1></List1>
+    </>
+  );
+}
+
+export default App;
